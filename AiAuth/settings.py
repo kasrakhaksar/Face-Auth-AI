6ba24669bbdc24a459b3d03b274fd4e27dbbe165
kasra khaksar <ksra13khaksar@gmail.com>
2026-06-02 03:32:26 +0330

    replace sqlite test

--- AiAuth/settings.py
+++ AiAuth/settings.py
@@ -85,16 +85,32 @@ WSGI_APPLICATION = 'AiAuth.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
+
+
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.getenv("DB_NAME", "db.sqlite3"),
+        # اگر می‌خوای دیتابیس تست جدا باشه:
+        "TEST": {
+            "NAME": "test_db.sqlite3",
+        }
     }
 }
+
+# بقیه تنظیماتت سر جای خودشون
 
 
 # Password validation
